39b0f5318e93ea6caa309a8dfbead74f169bee1d
Vincent <v_rohou@yahoo.fr>
2020-09-17 21:50:29 +0200

    Add public_ip information in 'where' requests Usage of 3rd party service ipify

--- my_ip_please.py
+++ my_ip_please.py
@@ -3,6 +3,7 @@ import socket
 import telegram
 import config
 from telegram.ext import CommandHandler, Updater
+from requests import get
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
@@ -16,12 +17,16 @@ def start(bot, update):
         bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
 def where(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=f'Chat_id: {update.message.chat_id}')
     if update.message.chat_id in config.CHATIDLIST:
         ip_list = socket.gethostbyname_ex(socket.gethostname())
         bot.send_message(chat_id=update.message.chat_id, text=f'Hostname: {ip_list[0]}')
         for number,ip in enumerate(ip_list[2]):
             msg = f'IP #{number + 1} - {ip}'
             bot.send_message(chat_id=update.message.chat_id, text=msg)
+        
+        public_ip = get('https://api.ipify.org').text
+        bot.send_message(chat_id=update.message.chat_id, text=f'Public IP: {public_ip}')
 
 def getid(bot, update):
         bot.send_message(chat_id=update.message.chat_id, text=str(update.message.chat_id))
